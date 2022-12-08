8f093c7eed10098567bb858be6c4c1aacbb1f4d8
Hubert <hubert.villeneuve@inrae.fr>
2022-12-08 15:33:29 +0100

    this version works

--- scripts/fixed.py
+++ scripts/fixed.py
@@ -11,6 +11,7 @@ from cv_bridge import CvBridge
 from std_msgs.msg import Header
 from geometry_msgs.msg import Pose, Quaternion, Point, PoseStamped
 from sensor_msgs.msg import Image
+from muav.msg import Tracking
 
 
 class Camera():
@@ -26,7 +27,8 @@ class BlobDetector(object):
 
   def detect(self, image):
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    target = rospy.get_param("target")
+    #target = rospy.get_param("target")
+    target = rospy.get_param("current_color")
     if target == "blue":
         mask = cv2.inRange(hsv_image, (90, 50, 70), (128, 255,255))
     else:
@@ -44,6 +46,8 @@ class FixedUAV():
         self.detector = BlobDetector()
         self.bridge = CvBridge()
         self.tracking_pose_cmd = rospy.Publisher('tracking/command/pose', PoseStamped, queue_size=1)
+        #publisher for custom msg
+        self.color_pub = rospy.Publisher('current_color', Tracking, queue_size=1)
 
 
     def process_image(self, image: Image):
@@ -52,15 +56,21 @@ class FixedUAV():
         target_center = self.detector.detect(cv_image)
         target_pose = -self.cam.Kinv @ target_center * self.cam.h
         if abs(target_pose[0]) < 10 and abs(target_pose[1]) < 10:
+            print(target_pose[0], target_pose[1])
             h = Header(); h.stamp = rospy.Time.now()
             pose = PoseStamped(header=h, pose=Pose(Point(target_pose[0].item(), target_pose[1].item(), 2), Quaternion(0, 0, 0, 1)))
             self.tracking_pose_cmd.publish(pose)
-        
+            msg = Tracking()
+            msg.header.stamp = rospy.Time.now()
+            msg.header.frame_id = rospy.get_param("current_color")
+            msg.X = target_pose[0]
+            msg.Y = target_pose[1]
+            self.color_pub.publish(msg)
 
 
 def main():
     rospy.init_node('fixed', anonymous=True)
-    fixed_position = Point(0, 0, 4)
+    fixed_position = Point(0, 0, 8)
     fixed_uav = FixedUAV(fixed_position)
     rospy.Subscriber('fixed/downward_cam/camera/image', Image, fixed_uav.process_image, queue_size=1)
     pose_cmd = rospy.Publisher('fixed/command/pose', PoseStamped, queue_size=100)
